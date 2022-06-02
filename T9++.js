719ce6fe6f8bc88cbbcf9134435dc7c41c96aa41
lrobt97 <lukerthomson@gmail.com>
2022-06-02 14:55:35 +0100

    Removed unused variables

--- T9++.js
+++ T9++.js
@@ -84,10 +84,6 @@ var getPublicationMultiplier = (tau) => tau.pow(0.159);
 var getPublicationMultiplierFormula = (symbol) => "{" + symbol + "}^{0.159}";
 var getTau = () => currency.value;
 var get2DGraphValue = () => currency.value.sign * (BigNumber.ONE + currency.value.abs()).log10().toNumber();
-
 var getQ1 = (level) => Utils.getStepwisePowerSum(level, 2, 10, 0);
 var getQ2 = (level) => BigNumber.TWO.pow(level);
-var getC1 = (level) => Utils.getStepwisePowerSum(level, 2, 10, 1);
-var getC2 = (level) => BigNumber.TWO.pow(level);
-var getC3 = (level) => BigNumber.TWO.pow(level);
 init();
